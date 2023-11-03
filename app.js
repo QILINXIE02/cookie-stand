9413f2676aa78f0e26c07e72c2011b76616ffafb
Qilin Xie <qxie@gicw.org>
2023-11-03 12:12:50 -0700

    footers

--- app.js
+++ app.js
@@ -61,3 +61,53 @@ for (const location of locations) {
   generateHourlySales(location);
   renderSalesData(location);
 }
+
+// Constructor function for Salmon Cookie Stand
+function CookieStand(location, minCustomers, maxCustomers, avgCookiesPerCustomer) {
+  this.location = location;
+  this.minCustomers = minCustomers;
+  this.maxCustomers = maxCustomers;
+  this.avgCookiesPerCustomer = avgCookiesPerCustomer;
+  this.cookiesSoldPerHour = []; // Store hourly sales
+}
+
+// Define your Salmon Cookie Stands
+const seattle = new CookieStand('Seattle', 23, 65, 6.3);
+const tokyo = new CookieStand('Tokyo', 3, 24, 1.2);
+const dubai = new CookieStand('Dubai', 11, 38, 3.7);
+const paris = new CookieStand('Paris', 20, 38, 2.3);
+const lima = new CookieStand('Lima', 2, 16, 4.6);
+
+// Implement the 'render' method for each Cookie Stand
+CookieStand.prototype.render = function() {
+  // Generate and append the row for this location to the sales table
+  const table = document.getElementById('sales-table');
+  const row = document.createElement('tr');
+  row.innerHTML = `<td>${this.location}</td>`;
+  // Generate hourly sales data and add to the row
+  // Calculate the daily total as you go
+  let dailyTotal = 0;
+  for (let i = 6; i <= 19; i++) { // 6:00 AM to 7:00 PM
+    const cookies = calculateCookiesForHour(i, this);
+    this.cookiesSoldPerHour.push(cookies);
+    row.innerHTML += `<td>${cookies} cookies</td>`;
+    dailyTotal += cookies;
+  }
+  row.innerHTML += `<td>${dailyTotal} cookies</td>`;
+  table.appendChild(row);
+};
+
+// Helper function to calculate cookies for a specific hour
+function calculateCookiesForHour(hour, stand) {
+  const customers = Math.floor(Math.random() * (stand.maxCustomers - stand.minCustomers + 1)) + stand.minCustomers;
+  return Math.round(customers * stand.avgCookiesPerCustomer);
+}
+
+// Render all Cookie Stands
+const cookieStands = [seattle, tokyo, dubai, paris, lima];
+for (const stand of cookieStands) {
+  stand.render();
+}
+
+// Additional logic to generate header and footer rows
+
